3f8273ef222ec69b60cf6a3a82ba10de35a9dc69
HD Moore <x@hdm.io>
2020-05-10 15:27:52 -0500

    change variable names to camelCase

--- cmd/inetdata-csv2mtbl/main.go
+++ cmd/inetdata-csv2mtbl/main.go
@@ -32,15 +32,15 @@ func main() {
 
 	flag.Usage = func() { usage() }
 
-	index_key := flag.Int("k", 1, "The field index to use as the key")
-	index_val := flag.Int("v", 2, "The field index to use as the value")
-	reverse_key := flag.Bool("r", false, "Store the key in reverse order")
-	max_fields := flag.Int("M", -1, "The maximum number of fields to parse with the delimiter")
+	indexKey := flag.Int("k", 1, "The field index to use as the key")
+	indexVal := flag.Int("v", 2, "The field index to use as the value")
+	reverseKey := flag.Bool("r", false, "Store the key in reverse order")
+	maxFields := flag.Int("M", -1, "The maximum number of fields to parse with the delimiter")
 	compression := flag.String("c", "snappy", "The compression type to use (none, snappy, zlib, lz4, lz4hc)")
 	delimiter := flag.String("d", ",", "The delimiter to use as a field separator")
-	sort_skip := flag.Bool("S", false, "Skip the sorting phase and assume keys are in pre-sorted order")
-	sort_tmp := flag.String("t", "", "The temporary directory to use for the sorting phase")
-	sort_mem := flag.Uint64("m", 1, "The maximum amount of memory to use, in gigabytes, for the sorting phase")
+	sortSkip := flag.Bool("S", false, "Skip the sorting phase and assume keys are in pre-sorted order")
+	sortTmp := flag.String("t", "", "The temporary directory to use for the sorting phase")
+	sortMem := flag.Uint64("m", 1, "The maximum amount of memory to use, in gigabytes, for the sorting phase")
 
 	version := flag.Bool("version", false, "Show the version and build timestamp")
 
@@ -58,22 +58,22 @@ func main() {
 
 	fname := flag.Args()[0]
 
-	sort_opt := mtbl.SorterOptions{Merge: mergeFunc, MaxMemory: 1000000000}
-	sort_opt.MaxMemory *= *sort_mem
-	if len(*sort_tmp) > 0 {
-		sort_opt.TempDir = *sort_tmp
+	sortOpt := mtbl.SorterOptions{Merge: mergeFunc, MaxMemory: 1000000000}
+	sortOpt.MaxMemory *= *sortMem
+	if len(*sortTmp) > 0 {
+		sortOpt.TempDir = *sortTmp
 	}
 
-	compression_alg, ok := inetdata.MTBLCompressionTypes[*compression]
+	compAlg, ok := inetdata.MTBLCompressionTypes[*compression]
 	if !ok {
 		fmt.Fprintf(os.Stderr, "Invalid compression algorithm: %s\n", *compression)
 		os.Exit(1)
 	}
 
-	s := mtbl.SorterInit(&sort_opt)
+	s := mtbl.SorterInit(&sortOpt)
 	defer s.Destroy()
 
-	w, we := mtbl.WriterInit(fname, &mtbl.WriterOptions{Compression: compression_alg})
+	w, we := mtbl.WriterInit(fname, &mtbl.WriterOptions{Compression: compAlg})
 	defer w.Destroy()
 
 	if we != nil {
@@ -88,41 +88,41 @@ func main() {
 	buf := make([]byte, 0, 128*1024)
 	scanner.Buffer(buf, len(buf)*2)
 
-	var current_line uint = 1
+	var cline uint = 1
 	for scanner.Scan() {
-		current_line++
+		cline++
 		raw := strings.TrimSpace(scanner.Text())
 		if len(raw) == 0 {
 			continue
 		}
 
-		bits := strings.SplitN(raw, *delimiter, *max_fields)
+		bits := strings.SplitN(raw, *delimiter, *maxFields)
 
-		if len(bits) < *index_key {
+		if len(bits) < *indexKey {
 			fmt.Fprintf(os.Stderr, "No key: %s\n", raw)
 			continue
 		}
 
-		if len(bits) < *index_val {
+		if len(bits) < *indexVal {
 			fmt.Fprintf(os.Stderr, "No value: %s\n", raw)
 			continue
 		}
 
-		kstr := bits[*index_key-1]
+		kstr := bits[*indexKey-1]
 		if len(kstr) == 0 {
 			continue
 		}
 
-		vstr := bits[*index_val-1]
+		vstr := bits[*indexVal-1]
 		if len(vstr) == 0 {
 			continue
 		}
 
-		if *reverse_key {
+		if *reverseKey {
 			kstr = inetdata.ReverseKey(kstr)
 		}
 
-		if *sort_skip {
+		if *sortSkip {
 			if e := w.Add([]byte(kstr), []byte(vstr)); e != nil {
 				fmt.Printf("Failed to add %v -> %v: %v\n", kstr, vstr, e)
 			}
@@ -134,11 +134,11 @@ func main() {
 	}
 
 	if err := scanner.Err(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error while processing line %d : %s\n", current_line, err)
+		fmt.Fprintf(os.Stderr, "Error while processing line %d : %s\n", cline, err)
 		os.Exit(1)
 	}
 
-	if !*sort_skip {
+	if !*sortSkip {
 		if e := s.Write(w); e != nil {
 			fmt.Fprintf(os.Stderr, "Error: %s\n", e)
 			os.Exit(1)
